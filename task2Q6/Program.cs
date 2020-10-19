9e9feff983886d3da2e7135fc14db69aed61f443
Nahid19-1 <nahidmarfy07@gmail.com>
2020-10-19 21:54:57 +0600

    Question 2& 6

---
+++ task2Q6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task2Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int fact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("Factorial is: "+fact);
+
+            Console.ReadLine();
+        }
+    }
+}
