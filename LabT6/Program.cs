fe8e102e5270d4fb05677ea6701687b0c63c9612
Nahid19-1 <nahidmarfy07@gmail.com>
2020-11-09 10:59:22 +0600

    labt6

--- LabT6/Program.cs
+++ LabT6/Program.cs
@@ -6,7 +6,12 @@ namespace LabT6
     {
         static void Main(string[] args)
         {
+            Library l = new Library("National","Dhaka");
+            Student s1 = new Student("Nahid","2020","CSE");
+            Student s2 = new Student("Sakib","2021","EEE");
+
             
+
         }
     }
 }
